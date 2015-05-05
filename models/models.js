fbb10f1515525c8cbeaac710245e82f6e1571c5b
claranieto <c.nieto-taladriz@alumnos.upm.es>
2015-05-05 14:53:59 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,22 @@
+var url = precess.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(DB_name, user, pwd,{
+	dialect: protocol,
+	protocol: protocol,
+	port: port,
+	host: host,
+	storage: storage,
+	omitNull: true
+}
+};
+
