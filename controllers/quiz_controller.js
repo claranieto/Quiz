1bef1e15328909f0e0bbcf2cdcdb1d5d3ab84ec5
claranieto <c.nieto-taladriz@alumnos.upm.es>
2015-05-12 16:16:09 +0200

    Cajetín de búsquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,25 +1,57 @@
 var models = require('../models/models.js');
 
+//Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res , next, quizId){
+    models.Quiz.find(quizId).then(
+        function(quiz){
+            if (quiz){
+                req.quiz = quiz;
+                next();
+            }else {next (new Error('No existe quizId=' +quizId));}
+        }
+    ).catch(function(error) {next(error);});
+};
+
+// GET /quizes
+exports.index = function(req, res, next){
+    if (req.query.search != null){ //se quiere buscar algo concreto
+        req.query.search = "%"+req.query.search+"%"; //quitar espacios
+        models.Quiz.findAll({where: ["pregunta like ?", req.query.search]})
+            .then(function (quizes){
+            
+                    for (i=0; i<quizes.length; i++){
+                        while( quizes[i].pregunta < quizes[i-1].pregunta){
+                            var p = quizes[i-1];
+                            quizes[i-1] = quizes[i];
+                            quizes[i] = p;
+                        }  
+                    }
+            
+                res.render('quizes/index', {quizes: quizes});
+            }
+        ).catch(function(error){ next(error);})
+            
+    }else{
+        models.Quiz.findAll().then(function(quizes){
+            res.render('quizes/index', {quizes: quizes});
+            }
+	    ).catch(function(error){next(error);})
+    }
+};
+
+
 //GET/quizes/:id
 exports.show = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
-	})
+		res.render('quizes/show', {quiz: req.quiz});
 };
 
 //GET/quizes/:idanswer
 exports.answer = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'/*, quizId: req.params.quizId*/});
-		}else
-		res.render ('quizes/answer', { quiz: quiz, respuesta: 'Incorrecto'/*, quizId: req.params.quizId*/});
-	})
+    var resultado = 'Incorrecto';
+		if (req.query.respuesta === req.quiz.respuesta){
+			resultado = 'Correcto';
+		}
+		res.render ('quizes/answer', { quiz: req.quiz, respuesta: resultado});
 };
 
-// GET /quizes
-exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});
-	})
-};
+//exports.searc
